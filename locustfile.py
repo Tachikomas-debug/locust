5cde84c9d115d92af338a48ef0a5c371826902e3
ddewada <ddewada@polysign.io>
2021-09-17 17:04:31 +0530

    locust test update

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,25 @@
-from locust import HttpUser, task
+# from locust import HttpUser, task
+#
+# class HelloWorldUser(HttpUser):
+#     @task
+#     def hello_world(self):
+#         self.client.get("http://google.com")
 
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("http://google.com")
+
+
+from locust import task, between, HttpUser, SequentialTaskSet
+
+class UserBehaviour(SequentialTaskSet):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    @task()
+    def create_task(self):
+        self.client.get("/")
+
+
+class MyUser(HttpUser):
+    wait_time = between(1, 2)
+    host = "http://google.com"
+    tasks = [UserBehaviour]
