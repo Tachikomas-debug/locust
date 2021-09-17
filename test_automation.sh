b6829c873d171abeb559a166dab7265082697ee7
ddewada <ddewada@polysign.io>
2021-09-17 15:34:44 +0530

    updted test_automation script

--- test_automation.sh
+++ test_automation.sh
@@ -1,13 +1,14 @@
-if [ -d "myenv" ];
-  then
-    echo "Virtual Environment Exist"
-    source myenv/bin/activate && pip install -r requirements.txt
-  else
-    echo "Creating Virtual Environment"
-    python -m venv myenv
-    source myenv/bin/activate && pip install -r requirements.txt
-fi
-
+#if [ -d "myenv" ];
+#  then
+#    echo "Virtual Environment Exist"
+#    source myenv/bin/activate && pip install -r requirements.txt
+#  else
+#    echo "Creating Virtual Environment"
+#    python -m venv myenv
+#    source myenv/bin/activate && pip install -r requirements.txt
+#fi
+echo "Installing dependencies"
+pip install -r requirements.txt
 echo "Test Started"
 # Run test case for 100 user for 10 mins
 locust -f locustfile.py -u 100 -r 1 --only-summary --headless --run-time=6000 --csv=example_for_100_user
