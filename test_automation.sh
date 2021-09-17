8f31f353f8aefaa47f9fe07e2682118d49ccd802
ddewada <ddewada@polysign.io>
2021-09-17 15:42:53 +0530

    cmd for pip upgrade

--- test_automation.sh
+++ test_automation.sh
@@ -7,8 +7,14 @@
 #    python -m venv myenv
 #    source myenv/bin/activate && pip install -r requirements.txt
 #fi
+
+echo "updating python env"
+pip install --upgrade pip
+pip install --upgrade setuptools
+
 echo "Installing dependencies"
 pip install -r requirements.txt
+
 echo "Test Started"
 # Run test case for 100 user for 10 mins
 locust -f locustfile.py -u 100 -r 1 --only-summary --headless --run-time=6000 --csv=example_for_100_user
